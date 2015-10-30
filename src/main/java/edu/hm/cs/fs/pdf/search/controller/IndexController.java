c87a0cc3c7dbf1ad5ef3e1b240599ae701ad4b74
Fabian Trampusch <fabian.trampusch@freenet.de>
2015-10-30 22:44:47 +0100

    Changed @RequestMapping for index page to explicit GET
    (triggering travis...)

--- src/main/java/edu/hm/cs/fs/pdf/search/controller/IndexController.java
+++ src/main/java/edu/hm/cs/fs/pdf/search/controller/IndexController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -20,7 +21,7 @@ public class IndexController {
     @Autowired
     private SearchService searchService;
     
-    @RequestMapping("/")
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     String index(Model model) {
         model.addAttribute("documentCount", searchService.getDocumentCount());
         return "index";
